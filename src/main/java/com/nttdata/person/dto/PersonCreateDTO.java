d1a9bcd3fbfa005a52ca1f5e622ac0589cc4c84d
Alexander Rosali Valerio Cruz <avaleric@emeal.nttdata.com>
2022-04-22 11:45:42 -0500

    Add validation fields

--- src/main/java/com/nttdata/person/dto/PersonCreateDTO.java
+++ src/main/java/com/nttdata/person/dto/PersonCreateDTO.java
@@ -1,6 +1,10 @@
 package com.nttdata.person.dto;
 
 import java.util.Date;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import com.nttdata.person.util.Constant;
 
 import lombok.Data;
@@ -8,14 +12,39 @@ import lombok.Data;
 @Data
 public class PersonCreateDTO {
 
-	@NotNull(message = Constant.EMPTY_NULL_FIELD)
+	@NotNull(message=Constant.EMPTY_NULL_FIELD)
+	@NotEmpty(message=Constant.EMPTY_NULL_FIELD)
 	private String firstName;
+	
+	@NotNull(message=Constant.EMPTY_NULL_FIELD)
+	@NotEmpty(message=Constant.EMPTY_NULL_FIELD)
 	private String lastName;
+	
+	@NotNull(message=Constant.EMPTY_NULL_FIELD)
+	@NotEmpty(message=Constant.EMPTY_NULL_FIELD)
 	private String documentType;
+	
+	@NotNull(message=Constant.EMPTY_NULL_FIELD)
+	@NotEmpty(message=Constant.EMPTY_NULL_FIELD)
 	private String document;
+	
+	@NotNull(message=Constant.EMPTY_NULL_FIELD)
+	@NotEmpty(message=Constant.EMPTY_NULL_FIELD)
 	private Date dateNac;
+	
+	@NotNull(message=Constant.EMPTY_NULL_FIELD)
+	@NotEmpty(message=Constant.EMPTY_NULL_FIELD)
 	private String email;
+	
+	@NotNull(message=Constant.EMPTY_NULL_FIELD)
+	@NotEmpty(message=Constant.EMPTY_NULL_FIELD)
 	private String iphone;
+	
+	@NotNull(message=Constant.EMPTY_NULL_FIELD)
+	@NotEmpty(message=Constant.EMPTY_NULL_FIELD)
 	private String address;
+	
+	@NotNull(message=Constant.EMPTY_NULL_FIELD)
+	@NotEmpty(message=Constant.EMPTY_NULL_FIELD)
 	private Date createdAt;
 }
